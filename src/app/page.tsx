bc197028a2914ee3234dea4ddfd67ce12ae3c085
DemwE <mateuszczarnecki360@gmail.com>
2025-01-11 20:12:11 +0100

    mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div id="countdownDisplay" className="grid grid-cols-4 gap-8 text-center font-sora select-none">
+      <div id="countdownDisplay" className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center font-sora select-none">
         <div className="min-w-24">
           <p className="text-6xl font-bold ">{timeRemaining.days}</p>
           <p>Days</p>
