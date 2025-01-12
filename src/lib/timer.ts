74bc25506d1b4a9fc4cfee282051912313b817ea
DemwE <mateuszczarnecki360@gmail.com>
2025-01-12 13:23:47 +0100

    nothing special

--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -1,4 +1,11 @@
-import { calculateTimeRemaining } from '@/lib/counter';
+export function calculateTimeRemaining(timeDiff: number) {
+  return {
+    days: Math.floor(timeDiff / (1000 * 60 * 60 * 24)),
+    hours: Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    minutes: Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60)),
+    seconds: Math.floor((timeDiff % (1000 * 60)) / 1000),
+  };
+}
 
 export function initializeTargetDate(setTargetDate: (date: string) => void, setThemeMode: (mode: 'light' | 'dark') => void) {
   const savedDate = localStorage.getItem('targetDate');
